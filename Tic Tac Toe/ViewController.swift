26c51c20c1240fe89fec4eacb41130c5589f58fa
Pratik Anilkumar Parmar <parm98@c43182m.local>
2017-10-18 10:11:31 +1300

    Initial Commit

---
+++ Tic Tac Toe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tic Tac Toe
+//
+//  Created by Pratik Anilkumar Parmar on 18/10/17.
+//  Copyright © 2017 Pratik Anilkumar Parmar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
